fc07b816806c6e67e815bd05f964424d1f814982
Ricardo Herrera <rherrerapetit@gmail.com>
2014-05-09 23:53:04 -0500

    Installing refinery cms

---
+++ qzarcms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qzarcms::Application.config.secret_token = '9c711d4528956e27745ddb71178baff6cb145643375a65e8a1a2bccff89bde0a75fdb66f9174b897959268553d8e02af9667ffaea10df377924a61cd50a449e6'
