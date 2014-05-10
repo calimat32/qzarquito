4821634b815c4c15c7ecbd7258a31f84331a399c
Ricardo Herrera <rherrerapetit@gmail.com>
2014-05-10 00:11:56 -0500

    Sucessfully installed cms and changed Company Name to Qzar Ecuador

---
+++ qzarcms/qzarcms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qzarcms::Application.config.secret_token = '1a9aa579ffeed1e4212d520039ead877b0782ed852fd39996b3a5d5138edd756f9bbe2f5e261b75768eb6524d64c1c8e81cd4df24621b6f6d162b5034ac262db'
